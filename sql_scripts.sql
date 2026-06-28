c38ac72b7c315114672f9ad18bb934c4353745ae
Userolha <pugacholhapgv@gmail.com>
2026-06-28 14:07:27 +0300

    fix problem in task 1, and add task 3 (Procedure: Add Product to Order)

--- sql_scripts.sql
+++ sql_scripts.sql
@@ -2,12 +2,14 @@
 --Task 1 — Function: Calculate Order Total
 
 create or replace function calculate_order_total(p_order_id int)
-language plpgsql;
-returns numeric as
+returns numeric
+language plpgsql
+as
 $$
 declare total numeric;
 begin
-	select coalesce(sum(oi.quantity*oi.price), 0) as total
+	select coalesce(sum(oi.quantity*oi.price), 0)
+	into total
 	from order_items as oi
 	where order_id = p_order_id;
 	return total;
@@ -30,6 +32,48 @@ begin
 	end if;
 	insert into orders(customer_id, order_date, total_amount)
 	values (p_customer_id, current_timestamp, 0);
+
+end;
+$$;
+
+-- Task 3 — Procedure: Add Product to Order
+
+create or replace procedure add_product_to_order(
+    p_order_id int,
+    p_product_id int,
+    p_quantity int
+)
+language plpgsql
+AS $$
+declare
+	intermediate_price numeric(10,2);
+	intermediate_stock_quantity int;
+begin
+	if  p_quantity <= 0
+-- перевірка щоб не додати нульове або від'ємне quantity
+		then
+			return;
+	end if;
+
+	select stock_quantity, price
+	into intermediate_stock_quantity, intermediate_price
+	from products
+	where product_id = p_product_id;
+
+	if intermediate_stock_quantity < p_quantity
+-- перевірка на Prevent adding a product if there is not enough stock.
+		then
+			return;
+	end if;
+
+	insert into order_items(order_id, product_id, quantity, price)
+	values (p_order_id, p_product_id, p_quantity, intermediate_price);
+--Use the current product price from the products table.
+
+	update products
+	set stock_quantity = stock_quantity -p_quantity
+--Decrease products.stock_quantity.
+	where product_id = p_product_id;
 
 end;
 $$;
